a43f156c6cc5fdae85965219fec3e3a6d3942324
Baaqar-007 <baaqarnaqi@gmail.com>
2023-12-22 21:53:19 +0530

    Add database

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
 # import necessary libraries
+from datetime import datetime
 from flask import Flask , render_template , flash, redirect , url_for 
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 # initialize flask app
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '33754a16e180c8a45c1b1cdf867a2157' # protects against forgery attacks , generated from "secrets"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique =True , nullable = False)
+    email = db.Column(db.String(120), unique =True , nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default = 'deafult.jpg')
+    password = db.Column(db.String(60),  nullable = False)
+    posts = db.relationship('Post', backref='author', lazy=True) # data is not loaded unless actually needed (lazy collection)
+    
+    def __repr__(self):
+        return f"User('{self.username}' , '{self.email}' , '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True) 
+    title = db.Column(db.String(100), nullable= False)
+    date_posted = db.Column(db.DateTime, nullable= False, default = datetime.utcnow) # no parenthesis cause we need the function not the time right now
+    content = db.Column(db.Text, nullable=False) 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    def __repr__(self):
+        return f"Post('{self.title}','{self.date_posted}')"
+    
+
+
+
+    
 
 # list of sample blog posts
 posts = [
