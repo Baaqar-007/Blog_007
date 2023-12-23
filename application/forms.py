911944266d96c76d98e7c935b98ab0448f42eccb
Baaqar-007 <baaqarnaqi@gmail.com>
2023-12-24 03:03:25 +0530

    Add create, update and delete functionality for posts.

--- application/forms.py
+++ application/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm 
 from flask_wtf.file import FileField,   FileAllowed
 from flask_login import current_user
-from wtforms import StringField , PasswordField, SubmitField, BooleanField
+from wtforms import StringField , PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length , Email, EqualTo, ValidationError
 from application.models import User
 
@@ -51,6 +51,13 @@ class UpdateAccountForm(FlaskForm):
         if email.data != current_user.email:
             if User.query.filter_by(email=email.data).first():
                 raise ValidationError('Your email has already been registered.')
+            
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+    
     
     
     
